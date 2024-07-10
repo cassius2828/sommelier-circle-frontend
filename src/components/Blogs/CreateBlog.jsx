d774f88c7448e7776bf22bc8fde45a85616d972f
Cassius <cassius.reynolds.dev@gmail.com>
2024-07-09 19:52:41 -0700

    added a script with global = globalThis to fix an issue with draft.js

--- src/components/Blogs/CreateBlog.jsx
+++ src/components/Blogs/CreateBlog.jsx
@@ -1,6 +1,11 @@
+import { Editor, EditorState } from "draft-js";
+import "draft-js/dist/Draft.css";
+import { useState } from "react";
+
 const CreateBlog = () => {
-  return (
-    <div>CreateBlog</div>
-  )
-}
-export default CreateBlog
+  const [editorState, setEditorState] = useState(() =>
+    EditorState.createEmpty()
+  );
+  return <Editor editorState={editorState} onChange={setEditorState} />;
+};
+export default CreateBlog;
